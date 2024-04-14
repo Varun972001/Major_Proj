d2fa8e685a117d2c91f6c77b9002f30d1fee96aa
Varun <varunjain972001@gmail.com>
2024-04-14 20:31:52 +0530

    Changes13

--- crop_sys1/crop_sys1/ml.py
+++ crop_sys1/crop_sys1/ml.py
@@ -10,11 +10,9 @@ def swap(final_data,i,j):
     return final_data
 
 def ml_func(final_data):
-    # try:
     with open('..\crop_sys1\crop_sys1\model1.pkl', 'rb') as file:
         model = pickle.load(file)
     print("Pickle File Creation successfull")
-        # index=[0]
     
     final_data=swap(final_data,3,5)
     final_data=swap(final_data,4,6)
@@ -25,12 +23,52 @@ def ml_func(final_data):
     prediction = model.predict(final_data1)
     print(prediction)
 
-        # if prediction==0:
-        #     print("The Is Not A Malware")
-        # else:
-        #     print("The Is A Malware")   
+    if prediction == 0:
+        return "apple"
+    elif prediction == 1:
+        return "banana"
+    elif prediction == 2:
+        return "blackgram"
+    elif prediction == 3:
+        return "chickpea"
+    elif prediction == 4:
+        return "coconut"
+    elif prediction == 5:
+        return "coffee"
+    elif prediction == 6:
+        return "cotton"
+    elif prediction == 7:
+        return "grapes"
+    elif prediction == 8:
+        return "jute"
+    elif prediction == 9:
+        return "kidneybeans"
+    elif prediction == 10:
+        return "lentil"
+    elif prediction == 11:
+        return "maize"
+    elif prediction == 12:
+        return "mango"
+    elif prediction == 13:
+        return "mothbeans"
+    elif prediction == 14:
+        return "mungbean"
+    elif prediction == 15:
+        return "muskmelon"
+    elif prediction == 16:
+        return "orange"
+    elif prediction == 17:
+        return "papaya"
+    elif prediction == 18:
+        return "pigeonpeas"
+    elif prediction == 19:
+        return "pomegranate"
+    elif prediction == 20:
+        return "rice"
+    elif prediction == 21:
+        return "watermelon"
+    
     return prediction
-    # except Exception as e:
-    #     print("Error Loading Pickle File")
-    #     return "Try Again!!"
+    
+    
     
--- index.py
+++ index.py
@@ -1,14 +1,7 @@
-#Data:n,p,k
-string="Data:180,89,56"
-result=[]
-result=string.split(":")
-result1=[]
-result1=result[1].split(",")
-print(result1)
-sum1=0
-sum1=int(result1[0])+int(result1[1])+int(result1[2])
-print(sum1)
-result2=[]
-for item in result1:
-    result2.append(round(int(item)/sum1,6))
-print(result2)
+keys = [20,11,3,9,18,13,14,2,10,19,1,12,7,21,15,0,16,17,4,6,8,5]
+values = ['rice','maize','chickpea','kidneybeans','pigeonpeas','mothbeans','mungbean','blackgram','lentil','pomegranate','banana','mango','grapes','watermelon','muskmelon','apple','orange','papaya','coconut','cotton','jute','coffee']
+
+# Use zip() to combine the lists into pairs of (key, value)
+combined_dict = dict(zip(keys, values))
+
+print(combined_dict)
